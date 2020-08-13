f88c29596bee48bf2302ba29c4e80371fdb1e457
Camila Nepomuceno <camila.cno@gmail.com>
2020-08-13 01:55:08 -0300

    Study page

---
+++ src/pages/TeacherList/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import TeacherItem from '../../components/TeacherItem';
+import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+import './styles.css';
+
+function TeacherList() {
+  return (
+    <div id="page-teacher-list" className="container">
+      <PageHeader title="Estes são os Proffys disponíveis.">
+        <form id="search-teachers">
+          <Input name="subject" label="Matéria" />
+          <Input name="week_day" label="Dia da semana" />
+          <Input type="time" name="time" label="Hora" />
+        </form>
+      </PageHeader>
+
+      <main>
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </main>
+    </div>
+  );
+}
+
+export default TeacherList;
