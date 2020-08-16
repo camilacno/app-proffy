e5d4c0f591f39578a37c1066fff84d7b9b84d3c8
Camila Nepomuceno <camila.cno@gmail.com>
2020-08-16 20:37:31 -0300

    Landing page

--- src/pages/Landing/styles.ts
+++ src/pages/Landing/styles.ts
@@ -20,8 +20,40 @@ const styles = StyleSheet.create({
     lineHeight: 30,
     marginTop: 80,
   },
-  bold: {
+  titleBold: {
     fontFamily: "Poppins_600SemiBold",
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+    marginTop: 40,
+    justifyContent: "space-between",
+  },
+  button: {
+    height: 150,
+    width: "48%",
+    backgroundColor: "#333",
+    borderRadius: 8,
+    padding: 24,
+    justifyContent: "space-between",
+  },
+  buttonPrimary: {
+    backgroundColor: "#9871f5",
+  },
+  buttonText: {
+    fontFamily: "Archivo_700Bold",
+    color: "#fff",
+    fontSize: 20,
+  },
+  buttonSecondary: {
+    backgroundColor: "#04d361",
+  },
+  totalConnections: {
+    fontFamily: "Poppins_400Regular",
+    color: "#d4c2ff",
+    fontSize: 12,
+    lineHeight: 20,
+    maxWidth: 140,
+    marginTop: 40,
   },
 });
 
