99989a4d3a2ee675c616007459ce70ca029a7494
Camila Nepomuceno <camila.cno@gmail.com>
2020-08-28 17:43:30 -0300

    Profile page structure and styling

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
 import TeacherList from './pages/TeacherList';
 import TeacherForm from './pages/TeacherForm';
 
@@ -13,6 +14,7 @@ function Routes() {
       <Route path="/" exact component={Landing} />
       <Route path="/login" component={Login} />
       <Route path="/signup" component={SignUp} />
+      <Route path="/profile" component={Profile} />
       <Route path="/study" component={TeacherList} />
       <Route path="/give-classes" component={TeacherForm} />
     </BrowserRouter>
