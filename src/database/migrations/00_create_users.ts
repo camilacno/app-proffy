7ddfdaadf2f6c787e048965d7b2f43a2cca99a90
Camila Nepomuceno <camila.cno@gmail.com>
2020-08-28 01:07:59 -0300

    User model update

--- src/database/migrations/00_create_users.ts
+++ src/database/migrations/00_create_users.ts
@@ -4,9 +4,14 @@ export async function up(knex: Knex) {
   return knex.schema.createTable('users', table => {
     table.increments('id').primary();
     table.string('name').notNullable();
-    table.string('avatar').notNullable();
-    table.string('whatsapp').notNullable();
-    table.string('bio').notNullable();
+    table.string('last_name');
+    table.string('email').notNullable();
+    table.string('password').notNullable();
+    table.boolean('is_proffy').notNullable();
+    table.string('avatar');
+    table.string('whatsapp');
+    table.string('bio');
+    table.timestamp('created_at').defaultTo(knex.fn.now());
   });
 }
 
